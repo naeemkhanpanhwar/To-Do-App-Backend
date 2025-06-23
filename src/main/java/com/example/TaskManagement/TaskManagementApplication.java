461e667b72fe7cd3584eca6d20c090a93e1049f4
Naeem Khan <naeemkhanpanhwar005@gmail.com>
2025-06-22 17:49:47 -0700

    fff

--- src/main/java/com/example/TaskManagement/TaskManagementApplication.java
+++ src/main/java/com/example/TaskManagement/TaskManagementApplication.java
@@ -10,4 +10,6 @@ public class TaskManagementApplication {
 		SpringApplication.run(TaskManagementApplication.class, args);
 	}
 
+	System.out.println("Task Management Application is running...");
+
 }
